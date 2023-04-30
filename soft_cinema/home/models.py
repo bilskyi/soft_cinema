8d737c3a29f8a5348c404e81b42f1963daac9d5f
bilskyi <bilskyioleksandr@outlook.com>
2023-04-30 13:12:52 +0300

    commit #6 / v1

--- soft_cinema/home/models.py
+++ soft_cinema/home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    poster = models.ImageField(upload_to='posters/')
+    age_limit = models.PositiveIntegerField()
+    year = models.PositiveIntegerField()
+    original_name = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    start_rental = models.DateField()  
+    end_rental = models.DateField()  
+    language = models.CharField(max_length=255)
+    duration = models.DurationField()
+    production = models.CharField(max_length=255)
+    studio = models.CharField(max_length=255)
+    starring = models.TextField()
+    description = models.TextField()
+    released = models.BooleanField()
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    
+    def __str__(self) -> str:
+        return self.name

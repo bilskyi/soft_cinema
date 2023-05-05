e230fbcaf9226fccfd2d9d974c283642b9111d9a
bilskyi <bilskyioleksandr@outlook.com>
2023-05-05 22:11:41 +0300

    register form

--- soft_cinema/soft_cinema/settings.py
+++ soft_cinema/soft_cinema/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'theater.apps.TheaterConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
--- soft_cinema/user/forms.py
+++ soft_cinema/user/forms.py
@@ -1,12 +1,16 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=255)
-    last_name = forms.CharField(max_length=255)
+    username = forms.CharField(max_length=255)
     email = forms.EmailField()
+    
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = ''
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1']

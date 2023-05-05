37b7941be63f982a621062a37b1035d99294b57c
bilskyi <bilskyioleksandr@outlook.com>
2023-05-05 17:11:43 +0300

    adding user system

--- soft_cinema/theater/urls.py
+++ soft_cinema/theater/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.theater, name='theater'),
+    path('buy-ticket/<slug:movie_slug>/', views.BuyTicket.as_view(), name='buy-ticket'),
 ]
--- soft_cinema/theater/views.py
+++ soft_cinema/theater/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from home.models import Movie
+from django.views.generic import DetailView
 
 def theater(request):
     return render(request, 'theater/theater.html')
 
+
+def buy_ticket(request, movie_slug):
+    movie = Movie.objects.filter(slug=movie_slug)
+    return render(request, 'theater/buy_ticket.html', {'movie': movie})
+
+
+class BuyTicket(DetailView):
+    model = Movie
+    template_name = 'theater/buy_ticket.html'
+    context_object_name = 'movie'
+    slug_url_kwarg = 'movie_slug'

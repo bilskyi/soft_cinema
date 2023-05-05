ce54b90d55bc926667d737ae11163e76156bff85
bilskyi <bilskyioleksandr@outlook.com>
2023-05-05 10:50:28 +0300

    sort by states

--- soft_cinema/home/urls.py
+++ soft_cinema/home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('movies/', views.Movie.as_view(), name='movies'),
     path('movies/<slug:movie_slug>/', views.MovieDetail.as_view(), name='movie_detail'),
-    path('movies/<slug:slug>/', views.StateList.as_view(), name='state_list'),
+    path('states/<slug:state_slug>/', views.StateList.as_view(), name='state_list'),
 ]
--- soft_cinema/home/views.py
+++ soft_cinema/home/views.py
@@ -21,16 +21,39 @@ class Home(MovieList, ListView):
 
 class Movie(MovieList, ListView):
     template_name = 'home/movies.html'
-    queryset = {'states'}
-
-
-class StateList(DetailView):
-    model = Movie
-    template_name = 'home/movies.html'
-    context_object_name = 'movie'
-
+    
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        states = self.object.state.all()
+        states = State.objects.all()
         context['states'] = states
         return context
+
+# class StateList(DetailView):
+#     model = Movie
+#     template_name = 'home/movies.html'
+#     context_object_name = 'movie'
+#     slug_url_kwarg = 'state_slug'
+
+
+#     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+#         context = super().get_context_data(**kwargs)
+#         states = self.object.state.all()
+#         #states = self.state.slug.all()
+#         context['states'] = states
+#         return context
+
+class StateList(ListView):
+    model = State
+    template_name = 'home/movies.html'
+    context_object_name = 'movies'
+
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        state_slug = self.kwargs['state_slug']
+        states = State.objects.all()
+        context['states'] = states
+        context['stage'] = self.model.objects.get(slug=state_slug)
+        return context
+

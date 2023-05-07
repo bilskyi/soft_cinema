e64549d13edbc1cfee62a7f248a753106edbd669
bilskyi <bilskyioleksandr@outlook.com>
2023-05-07 14:12:05 +0300

    adding user profile

--- soft_cinema/user/models.py
+++ soft_cinema/user/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    pass
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.username

e12bd45ae545dd6d49bd81163429a454cd2eaf3c
bilskyi <bilskyioleksandr@outlook.com>
2023-05-11 17:18:30 +0300

    try change form

--- soft_cinema/theater/models.py
+++ soft_cinema/theater/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class Seat(models.Model):
-    row = models.CharField(max_length=1)
-    seat = models.PositiveSmallIntegerField()
-    available = models.BooleanField(default=True)
+    seat_number = models.PositiveSmallIntegerField()
+    is_available = models.BooleanField(default=True)
 
     def __str__(self) -> str:
-        return f"Row {self.row}, Seat {self.seat}, Available: {self.available}"
+        return self.seat_number
--- soft_cinema/theater/views.py
+++ soft_cinema/theater/views.py
@@ -9,30 +9,12 @@ def theater(request):
     return render(request, 'theater/theater.html')
 
 
-class BuyTicket(DetailView):
-    model = Movie
-    template_name = 'theater/buy_ticket.html'
-    context_object_name = 'movie'
-    slug_url_kwarg = 'movie_slug'
-
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context['form'] = SeatForm
-        context['row'] = Seat.objects.values('row').distinct()
-        context['seat'] = Seat.objects.values('seat').distinct()
-        return context
-
-
 def buy_ticket(request, movie_slug):
     movie = Movie.objects.get(slug=movie_slug)
     form = SeatForm()
-    row = Seat.objects.values('row').distinct()
-    seat = Seat.objects.values('seat').distinct()
     context = {
         'movie': movie,
         'form': form,
-        'row': row,
-        'seat': seat,
     }
     if request.method == 'POST':
         form = SeatForm(request.POST)
